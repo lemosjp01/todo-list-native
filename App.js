0e382014d57de6441e4f20051cce7af96c015e22
lemosjp01 <33187777+lemosjp01@users.noreply.github.com>
2018-04-23 19:50:30 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Main from './app/components/Main';
+
+export default class App extends React.component {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
